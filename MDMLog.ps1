da14b86824e244f14320734afec196bb9accebcc
Carl T <carl-home@tashian.com>
2023-11-14 18:32:26 -0800

    Rev2

--- MDMLog.ps1
+++ MDMLog.ps1
@@ -1,3 +1,8 @@
+param (
+    [switch]$errorsOnly,
+    [string]$searchString
+)
+
 # Specify the event log and the number of events to retrieve
 $logName = "Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Admin"
 $numberOfEvents = 50
@@ -7,13 +12,18 @@ $events = Get-WinEvent -LogName $logName -MaxEvents $numberOfEvents | Sort-Objec
 
 # Display details of each event
 foreach ($event in $events) {
-    Write-Host "Event ID: $($event.Id) | Time Created: $($event.TimeCreated) | Provider Name: $($event.ProviderName) | Level: $($event.LevelDisplayName)"
+    # Check if only errors should be displayed
+    # Check if a search string is provided and the message contains the search string
+    if ((-not $errorsOnly -or $event.LevelDisplayName -eq "Error") -and (-not $searchString -or $event.Message -like "*$searchString*")) {
+        $levelEmoji = if ($event.LevelDisplayName -eq "Information") { "🆗" } else { "🙅" }
+        Write-Host "$levelEmoji $($event.LevelDisplayName) $($event.TimeCreated) $($event.Id)"
 
-    # Display additional lines for the message
-    $messageLines = $event.Message -split "`r`n"
-    foreach ($line in $messageLines) {
-        Write-Host "  $line"
+        # Display additional lines for the message
+        $messageLines = $event.Message -split "`r`n"
+        foreach ($line in $messageLines) {
+            Write-Host "  $line"
+        }
+
+        Write-Host "`n"  # Add a newline for better readability
     }
-
-    Write-Host "`n"  # Add a newline for better readability
 }
