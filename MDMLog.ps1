19058fa7233097a0e3031f50fdd16fe2a2530a81
Carl T <carl-home@tashian.com>
2023-11-14 18:42:56 -0800

    No emojis

--- MDMLog.ps1
+++ MDMLog.ps1
@@ -7,9 +7,6 @@ param (
 $logName = "Microsoft-Windows-DeviceManagement-Enterprise-Diagnostics-Provider/Admin"
 $numberOfEvents = 50
 
-# Specify if only errors should be displayed
-$errorsOnly = $false
-
 # Retrieve the most recent 50 events from the specified log
 $events = Get-WinEvent -LogName $logName -MaxEvents $numberOfEvents | Sort-Object TimeCreated -Descending
 
@@ -18,8 +15,7 @@ foreach ($event in $events) {
     # Check if only errors should be displayed
     # Check if a search string is provided and the message contains the search string
     if ((-not $errorsOnly -or $event.LevelDisplayName -eq "Error") -and (-not $searchString -or $event.Message -like "*$searchString*")) {
-        $levelEmoji = if ($event.LevelDisplayName -eq "Information") { "`u{1F197}" } else { "`u{1F645}" }
-        Write-Host "$levelEmoji $($event.LevelDisplayName) $($event.TimeCreated) $($event.Id)"
+        Write-Host "$($event.LevelDisplayName) $($event.TimeCreated) $($event.Id)"
 
         # Display additional lines for the message
         $messageLines = $event.Message -split "`r`n"
