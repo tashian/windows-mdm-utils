6c2ac961ac5f9e324e84522ad66bf19108e82992
Carl T <carl-home@tashian.com>
2023-11-14 18:03:05 -0800

    Next version

--- ListUserCerts.ps1
+++ ListUserCerts.ps1
@@ -1,8 +1,10 @@
-# Get all certificates from the CurrentUser\Root store
-$certificates = Get-ChildItem -Path Cert:\CurrentUser\Root
+param (
+    [string]$searchString = $null
+)
 
-# Display certificate information
-foreach ($cert in $certificates) {
+# Function to display certificate information
+function Show-CertificateInfo($cert, $storeLocation) {
+    Write-Host "Trust Store: $storeLocation"
     Write-Host "Subject: $($cert.Subject)"
     Write-Host "Issuer: $($cert.Issuer)"
     Write-Host "Thumbprint: $($cert.Thumbprint)"
@@ -10,4 +12,21 @@ foreach ($cert in $certificates) {
     Write-Host "Not Before: $($cert.NotBefore)"
     Write-Host "Not After: $($cert.NotAfter)"
     Write-Host "`n"  # Add a newline for better readability
+}
+
+# Get certificates from both CurrentUser and LocalMachine stores
+$stores = @("CurrentUser\Root", "CurrentUser\CA", "CurrentUser\My", "LocalMachine\Root", "LocalMachine\CA", "LocalMachine\My")
+
+foreach ($storePath in $stores) {
+    $storeLocation = $storePath -replace "(.*?)\\.*", '$1'
+    $storeName = $storePath -replace ".*?\\(.*)", '$1'
+    
+    $certificates = Get-ChildItem -Path "Cert:\$storePath"
+
+    foreach ($cert in $certificates) {
+        # If a search string is provided and the subject contains the search string (case-insensitive), display the certificate
+        if (-not $searchString -or $cert.Subject -like "*$searchString*") {
+            Show-CertificateInfo -cert $cert -storeLocation $storeLocation
+        }
+    }
 }
